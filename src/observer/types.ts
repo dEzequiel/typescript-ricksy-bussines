fbf2504c027cc940450808b30811629a82d4def6
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-09-04 17:41:03 +0200

    Observer pattern: subscriber and notifier common interface

--- src/observer/types.ts
+++ src/observer/types.ts
@@ -1,6 +1,4 @@
-import { Crystal } from './../subscribers/CrystalExpender';
 import { CreditCard } from "../CreditCard";
-import { Ufo } from "../subscribers/UfosPark";
 
 interface GuestDispatcher {
     dispatch(card: CreditCard): void
