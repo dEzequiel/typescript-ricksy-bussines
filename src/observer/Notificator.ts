68231ce5d9b846c60cf5d59f34e7f1ee253cc683
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-09-04 17:41:28 +0200

    Observer pattern: Notifier class

---
+++ src/observer/Notificator.ts
@@ -0,0 +1,17 @@
+import { CreditCard } from "../CreditCard";
+import { GuestDispatcher } from "./types";
+
+export class Notificator implements GuestDispatcher {
+
+  subscribers: GuestDispatcher[] = [];
+
+  register(service: GuestDispatcher): void {
+    this.subscribers.push(service);
+  }
+
+  dispatch(card: CreditCard): void {
+    this.subscribers.forEach(function (s: GuestDispatcher) {
+      s.dispatch(card)
+    })
+  }
+}
